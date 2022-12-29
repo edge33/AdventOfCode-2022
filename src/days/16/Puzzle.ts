8bf83d685e0e1a5aeec82627e4b3b3ef3c704d9f
Francesco Maida <francesco@email.com>
2022-12-29 18:49:10 +0100

    Re-implemented day 16

--- src/days/16/Puzzle.ts
+++ src/days/16/Puzzle.ts
@@ -1,213 +1,155 @@
-import path = require('path');
+/**
+ * Built from an idea got in the AoC subreddit
+ */
 import Puzzle from '../../types/AbstractPuzzle';
 
-// This is basic pathfinding just to find the time to travel from any valid valve to any other valid valve. Important to prevent moving unproductively into worthless rooms
-const getLowestCost = (
-  start: string,
-  end: string,
-  valveMap: {
-    [k: string]: {
-      readonly flowRate: number;
-      readonly tunnelConnections: string[];
-    };
-  }
-) => {
-  const queue: { room: string; cost: number; visited: string[] }[] = [
-    { room: start, cost: 0, visited: [start] },
-  ];
-  while (queue.length) {
-    const { room, cost, visited } = queue.shift()!;
-    if (room === end) {
-      return cost;
-    }
-    const { tunnelConnections } = valveMap[room];
-    if (tunnelConnections.includes(end)) {
-      return cost + 1;
-    }
-    tunnelConnections.forEach((tunnel) => {
-      if (!visited.includes(tunnel)) {
-        queue.push({
-          room: tunnel,
-          cost: cost + 1,
-          visited: [...visited, tunnel],
-        });
-      }
-    });
-  }
-  return -1;
+type Valve = {
+  flow: number;
+  adjs: string[];
 };
 
-// I actually found that it was faster to just generate all the possible routes within the time and then process their scores separately as opposed to pathing and calculating as I went. This is mainly because of how Part 2 works, this just allowed more control over ensuring things could be memoized and be smooth enough
-const makeAllPaths = (
-  time: number,
+type ValveMap = {
+  [k: string]: Valve;
+};
+
+const buildPath = (
+  path: string[],
+  valves: string[],
+  cost: number,
   pathCosts: {
     [k: string]: {
       [k: string]: number;
     };
   },
-  allValves: string[]
+  paths: Set<string>,
+  maxCost = 30
 ) => {
-  const pathList: Set<string> = new Set();
-  const getRemainingPath = (
-    steps: string[],
-    left: string[],
-    costThusFar: number
-  ): void => {
-    const last = steps[steps.length - 1];
-    if (!left.length) {
-      pathList.add(steps.join('-'));
-    }
-    return left.forEach((next) => {
-      const cost = pathCosts[last][next];
-      if (cost + 1 + costThusFar >= time) {
-        return pathList.add(steps.join('-'));
-      }
-      return getRemainingPath(
-        [...steps, next],
-        left.filter((pos) => pos !== next),
-        costThusFar + cost + 1
-      );
-    });
-  };
-  getRemainingPath(['AA'], [...allValves], 0);
-  return [...pathList].map((path) => path.split('-'));
-};
-
-// Takes paths and provides the amount of pressure they can release in the given time. This memoizes paths past a certain point so that the tail end of paths can be sped up. Part 1 simply consists of getting the best path for a single actor moving through and turning valves
-const scorePaths = (
-  paths: string[][],
-  time: number,
-  pathCosts: {
-    [k: string]: {
-      [k: string]: number;
-    };
-  },
-  valveMap: {
-    [k: string]: {
-      readonly flowRate: number;
-      readonly tunnelConnections: string[];
-    };
+  if (!valves.length) {
+    return paths.add(path.join(','));
   }
-) => {
-  const scorePath = (
-    opened: string[],
-    path: string[],
-    timeLeft: number,
-    debug = false
-  ): number => {
-    const nextStep = path[0];
-    const remainingSteps = path.slice(1);
-    const nextStepCost = pathCosts[opened[0]][nextStep];
-    const flowForStep =
-      opened[0] === 'AA' ? 0 : valveMap[opened[0]].flowRate * timeLeft;
-    if (!path.length) {
-      return flowForStep;
+  const last = path[path.length - 1];
+  for (const next of valves) {
+    const nextCost = pathCosts[last][next];
+    if (cost + nextCost + 1 >= maxCost) {
+      paths.add(path.join(','));
+      continue;
     }
-    const pressureReleased = scorePath(
-      [path[0], ...opened],
-      remainingSteps,
-      timeLeft - nextStepCost - 1,
-      debug
+
+    buildPath(
+      [...path, next],
+      valves.filter((v) => v !== next),
+      cost + nextCost + 1,
+      pathCosts,
+      paths,
+      maxCost
     );
-    return pressureReleased + flowForStep;
-  };
-  const pathScores = paths
-    .map(
-      (path) =>
-        [path, scorePath([path[0]], path.slice(1), time)] as [string[], number]
-    )
-    .sort(([_, valA], [__, valB]) => valB - valA);
-  return pathScores;
+  }
 };
 
-// Part 2 gives less time, but you have 2 actors moving through the caves. Since there are too many valves for even two actors to turn them all, one of the best ways to handle this is to just get the scores for all possible paths for a single actor and then find the best two that don't overlap to send the two actors to handle separately.
-const getBestCombo = (
+const buildAllPaths = (
+  valves: string[],
   pathCosts: {
     [k: string]: {
       [k: string]: number;
     };
   },
-  valveMap: {
+  maxCost = 30
+) => {
+  const paths: Set<string> = new Set();
+  buildPath(['AA'], [...valves], 0, pathCosts, paths, maxCost);
+  return [...paths].map((path) => path.split(','));
+};
+
+const getPathCost = (start: string, end: string, valveMap: ValveMap) => {
+  const toVisit = [{ node: start, cost: 0 }];
+  const visited = [start];
+  while (toVisit.length) {
+    const visiting = toVisit.shift();
+    if (visiting.node === end) {
+      return visiting.cost;
+    }
+    const ajds = valveMap[visiting.node].adjs;
+    for (const currentAdj of ajds) {
+      if (!visited.includes(currentAdj)) {
+        toVisit.push({ node: currentAdj, cost: visiting.cost + 1 });
+        visited.push(currentAdj);
+      }
+    }
+  }
+};
+
+const scorePath = (
+  path: string[],
+  valveMap: ValveMap,
+  pathCosts: {
     [k: string]: {
-      readonly flowRate: number;
-      readonly tunnelConnections: string[];
+      [k: string]: number;
     };
   },
-  allValves: string[]
+  time = 30
 ) => {
-  const pathScores = scorePaths(
-    makeAllPaths(26, pathCosts, allValves),
-    26,
-    pathCosts,
-    valveMap
-  );
-  const candidates = pathScores
-    .filter(([_, score]) => score > 0)
-    .map(([path, score]) => [path.slice(1), score] as [string[], number]);
-  const bestCombo = candidates.reduce((best, [path, score], i) => {
-    if (score < ((best / 2) | 0)) {
-      return best;
+  let score = 0;
+  let flow = 0;
+  let from = path.shift();
+  let openValveAtTime = -1;
+  let canOpenValve = true;
+  let newFlow = 0;
+  for (let t = 0; t < time; t++) {
+    if (path.length && canOpenValve) {
+      const next = path.shift();
+      openValveAtTime += pathCosts[from][next] + 1;
+      newFlow = valveMap[next].flow;
+      from = next;
+      canOpenValve = false;
     }
-    const splitPoint =
-      best === 0
-        ? undefined
-        : Math.max(
-            0,
-            candidates.findIndex((candidate) => candidate[1] + score < best) +
-              1 -
-              i
-          );
-
-    const noOverlap = candidates
-      .slice(i, splitPoint)
-      .find((helper) => helper[0].every((valve) => !path.includes(valve)));
-    if (!noOverlap) {
-      return best;
+    score += flow;
+    if (t === openValveAtTime && !canOpenValve) {
+      canOpenValve = true;
+      flow += newFlow;
+      newFlow = 0;
     }
-    return Math.max(best, noOverlap[1] + score);
-  }, 0);
-  return bestCombo;
+  }
+  return score;
 };
 
 export default class ConcretePuzzle extends Puzzle {
   public solveFirst(): string {
-    const valveMap = Object.fromEntries(
+    const valveMap: ValveMap = Object.fromEntries(
       this.input
         .split('\n')
         .filter(Boolean)
         .map((line) => {
           const [valve, tunnels] = line.split(';');
           const label = valve.match(/([A-Z]{2})/)?.[1] ?? 'broken';
-          const flowRate = Number(valve.match(/(\d+)/)?.[1] ?? 0);
-          const tunnelConnections = Array.from(
-            tunnels.matchAll(/([A-Z]{2})/g)
-          ).map((match) => match[1]);
-          return [label, { flowRate, tunnelConnections }] as const;
+          const flow = Number(valve.match(/(\d+)/)?.[1] ?? 0);
+          const adjs = Array.from(tunnels.matchAll(/([A-Z]{2})/g)).map(
+            (match) => match[1]
+          );
+          return [label, { flow, adjs }] as const;
         })
     );
-
-    // Rooms with no flowRate aren't worth considering as node worth passing through. Those with flowrate are valid nodes
-    const allValves = Object.entries(valveMap)
-      .map(([valve, { flowRate }]) => (flowRate > 0 ? valve : null))
-      .filter(Boolean) as string[];
+    const valves = Object.entries(valveMap)
+      .map(([valve, { flow }]) => (flow > 0 ? valve : undefined))
+      .filter(Boolean);
 
     const pathCosts = Object.fromEntries(
-      ['AA', ...allValves].map((start) => [
-        start,
+      ['AA', ...valves].map((valve) => [
+        valve,
         Object.fromEntries(
-          allValves
-            .filter((end) => end !== start)
-            .map((end) => [end, getLowestCost(start, end, valveMap)])
+          valves
+            .filter((v) => v !== valve)
+            // .map((v) => ({ to: v, cost: getPathCost(valve, v, valveMap) })),
+            .map((v) => [v, getPathCost(valve, v, valveMap)])
         ),
       ])
     );
 
-    return scorePaths(
-      makeAllPaths(30, pathCosts, allValves),
-      30,
-      pathCosts,
-      valveMap
-    )[0][1].toString();
+    const paths = buildAllPaths(valves, pathCosts);
+    const withScores = paths.map((path) => [
+      [...path],
+      scorePath([...path], valveMap, pathCosts),
+    ]);
+    return withScores.sort((a, b) => (a[1] > b[1] ? -1 : 1))[0][1].toString();
   }
 
   public getFirstExpectedResult(): string {
@@ -215,37 +157,61 @@ export default class ConcretePuzzle extends Puzzle {
   }
 
   public solveSecond(): string {
-    const valveMap = Object.fromEntries(
+    const valveMap: ValveMap = Object.fromEntries(
       this.input
         .split('\n')
         .filter(Boolean)
         .map((line) => {
           const [valve, tunnels] = line.split(';');
           const label = valve.match(/([A-Z]{2})/)?.[1] ?? 'broken';
-          const flowRate = Number(valve.match(/(\d+)/)?.[1] ?? 0);
-          const tunnelConnections = Array.from(
-            tunnels.matchAll(/([A-Z]{2})/g)
-          ).map((match) => match[1]);
-          return [label, { flowRate, tunnelConnections }] as const;
+          const flow = Number(valve.match(/(\d+)/)?.[1] ?? 0);
+          const adjs = Array.from(tunnels.matchAll(/([A-Z]{2})/g)).map(
+            (match) => match[1]
+          );
+          return [label, { flow, adjs }] as const;
         })
     );
-
-    // Rooms with no flowRate aren't worth considering as node worth passing through. Those with flowrate are valid nodes
-    const allValves = Object.entries(valveMap)
-      .map(([valve, { flowRate }]) => (flowRate > 0 ? valve : null))
-      .filter(Boolean) as string[];
+    const valves = Object.entries(valveMap)
+      .map(([valve, { flow }]) => (flow > 0 ? valve : undefined))
+      .filter(Boolean);
 
     const pathCosts = Object.fromEntries(
-      ['AA', ...allValves].map((start) => [
-        start,
+      ['AA', ...valves].map((valve) => [
+        valve,
         Object.fromEntries(
-          allValves
-            .filter((end) => end !== start)
-            .map((end) => [end, getLowestCost(start, end, valveMap)])
+          valves
+            .filter((v) => v !== valve)
+            // .map((v) => ({ to: v, cost: getPathCost(valve, v, valveMap) })),
+            .map((v) => [v, getPathCost(valve, v, valveMap)])
         ),
       ])
     );
-    return getBestCombo(pathCosts, valveMap, allValves).toString();
+
+    const paths = buildAllPaths(valves, pathCosts, 26);
+
+    const withScores: [string[], number][] = paths
+      .map(
+        (path) =>
+          [[...path], scorePath([...path], valveMap, pathCosts, 26)] as [
+            string[],
+            number
+          ]
+      )
+      .filter((items) => items[1] > 0)
+      .sort((a, b) => (a[1] >= b[1] ? -1 : 1));
+
+    return withScores
+      .reduce((best, [path, score], i) => {
+        const others = withScores.slice(i);
+        const nonOverlapping = others.find((entry) =>
+          path.slice(1).every((p) => !entry[0].slice(1).includes(p))
+        );
+        if (!nonOverlapping) {
+          return best;
+        }
+        return Math.max(best, score + nonOverlapping[1]);
+      }, 0)
+      .toString();
   }
 
   public getSecondExpectedResult(): string {
